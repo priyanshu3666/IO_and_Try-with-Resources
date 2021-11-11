539c65ea3bb294cbf3cab55537835162c472dcd1
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-11-11 15:04:18 +0530

    implements write() method and hands on

---
+++ src/com/io/WriteDemo.java
@@ -0,0 +1,12 @@
+package com.io;
+
+// Demonstrate System.out.write()
+public class WriteDemo {
+    public static void main(String[] args) {
+        int asciiValue;
+        asciiValue = 'A';
+        System.out.write(asciiValue);
+        System.out.write('\n');
+
+    }
+}
