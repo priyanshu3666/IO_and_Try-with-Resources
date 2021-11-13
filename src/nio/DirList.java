f53a61d4f750d82c7c5bffa320dec8ed7b7fb058
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-11-13 08:37:37 +0530

    implementing DirList

---
+++ src/nio/DirList.java
@@ -0,0 +1,30 @@
+package nio;
+
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+class DirList {
+    public static void main(String args[]) {
+        String dirname = "\\MyDir";
+        try (DirectoryStream<Path> dirstrm = Files.newDirectoryStream(Path.of(dirname))) {
+            System.out.println("Directory of " + dirname);
+            for (Path entry : dirstrm) {
+                BasicFileAttributes attribs = Files.readAttributes(entry, BasicFileAttributes.class);
+                if (attribs.isDirectory())
+                    System.out.print("<DIR>");
+                else
+                    System.out.print("      ");
+
+                System.out.println(entry.getName(1));
+
+            }
+        } catch (InvalidPathException e) {
+            System.out.println("Path Error " + e);
+        } catch (NotDirectoryException e) {
+            System.out.println(dirname + " is not a directory.");
+        } catch (IOException e) {
+            System.out.println("I/O Error: " + e);
+        }
+    }
+}
